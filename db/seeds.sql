f06f01f61ccabbb79ed87a612750c207585004b6
thegetoutgirl <amyahaha@gmail.com>
2020-04-14 01:24:47 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Bleu Cheese Burger", "false");
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Pizza Burger", "false");
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Samburger", "false");
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Beyond Burger", "false");
