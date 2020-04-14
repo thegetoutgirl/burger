d212b5885d5ab4bc6b94ce8300d56f1c4806e6a9
thegetoutgirl <amyahaha@gmail.com>
2020-04-14 02:43:37 -0400

    added model/controller info

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const router = express.Router();
+
+const burger = require('models/burger.js');
+
+// Get all burgers
+router.get('/', function (req, res){
+    burger.all(function(data) {
+        var hbarObj = {
+            burgers: data
+        };
+    console.log(hbarsObj);
+    res.render("index", hbarObj);
+    });
+
+});
+
+// Add a new burger
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+      "name", "devoured"
+    ], [
+      req.body.name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+// Update a burger
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  // Export routes for server.js to use.
+  module.exports = router;
+  
